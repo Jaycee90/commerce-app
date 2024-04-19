cb42d5d7a0529ca1458a93fce91970510b2c43fa
Jayce Turambe <jnn56@txstate.edu>
2024-04-19 16:58:48 -0500

    edited file path

--- src/home/AppSection.jsx
+++ src/home/AppSection.jsx
@@ -31,8 +31,8 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Image1 from '../images/01.jpg';
-import Image2 from '../images/02.jpg';
+import Image1 from '../../public/images/01.jpg';
+import Image2 from '../../public/images/02.jpg';
 
 const btnText = "Sign up for Free";
 const title = "Shop Anytime, Anywhere";
